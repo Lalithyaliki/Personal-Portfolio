97baa8bc4a4c9c9cda8643e677c303010c85ad4f
A-Harshavardhan <ananthamharsha333@gmail.com>
2025-03-22 19:23:09 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,16 @@
+
+function togglemenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+};
+
+
+
+
+
+
+const but=document.getElementById('button');
+const hom=document.getElementById('home-sec');
+
+but.addEventListener('click',function() {
+    hom.scrollIntoView({behavior : 'smooth'});
+});
