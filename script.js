0da139b7d6ecc224356ef0e819318e796d1399f1
Lalithyaliki <ananthaneniliki25@gmail.com>
2025-04-12 20:16:53 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,81 @@
 
+// toggle nav-links
+
 function togglemenu() {
     document.querySelector('.nav-links').classList.toggle('active');
 };
 
-const but=document.getElementById('button');
-const hom=document.getElementById('home-sec');
+function onhover() {
+    document.querySelector('.nav-links').classList.remove('active');
+};
 
-but.addEventListener('click',function() {
-    hom.scrollIntoView({behavior : 'smooth'});
+// button scrolling function
+
+let but = document.getElementById("button");
+but.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+        but.style.display = "block";
+    }
+    else {
+        but.style.display = "none";
+    }
+});
+
+// working of chat box (opening and closing)
+
+let icon = document.getElementById("icon-1");
+let chatbox = document.getElementById("chat");
+let closebox = document.getElementById("close-chat")
+
+icon.addEventListener("click", function () {
+    chatbox.classList.add("open");
+    but.classList.add("hide");
+});
+closebox.addEventListener("click", function () {
+    chatbox.classList.remove("open");
+    but.classList.remove("hide");
+});
+
+// getting details of person from the chat box
+
+let email = document.getElementById("email");
+let popupmessage = document.getElementById("pop-msg")
+
+document.getElementById("pop").addEventListener("click", function () {
+
+    if (email.value.trim() != '') {
+        popupmessage.innerHTML =`your details are accessed successfully ${email.value}.`;
+
+        popupmessage.classList.add("success");
+
+        setTimeout(() => {
+            popupmessage.classList.remove('success');
+        }, 3000);
+
+        document.getElementById("chat").classList.remove("open");
+    }
+    else {
+        popupmessage.innerHTML="enter valid email";
+        popupmessage.classList.add("success");
+    
+        setTimeout(() => {
+            popupmessage.classList.remove("success");
+        }, 3000);
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
